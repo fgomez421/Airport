70c2ebd60fe201c9c5610a6aeccc2f6ee9a91ea9
wweemsec <elicookweems@gmail.com>
2026-04-15 11:09:40 -0400

    start of airport

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 #main for the Airport project 
 print("Program start")
+filename = input("Enter a filename: ")
+f = open(filename, "r")
+
+valid_rows = 0
+invalid_rows = 0
+
+for line in f:
+    line = line.strip()
+    fields = line.split(",")
+
+    if len(fields) != 9:
+        invalid_rows = invalid_rows + 1 
+        print("Invalid row: ", line)
+        continue
+    
+    try:
+        year = int(fields[0])
+    except:
+        invalid_rows = invalid_rows + 1 
+        print("Invalid row: ", line)
+        continue
+
+    if year != 2024:
+        invalid_rows = invalid_rows + 1
+        print("Invalid row: ", line)
+        continue
